aed429e6905cfc9ed28c791cab7518fa97ccb12e
rNix <rnix@yandex.ru>
2018-11-17 14:35:47 +0700

    Add films-app

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -3,14 +3,23 @@
 set -e
 
 docker-compose up -d --build
+
 docker-compose exec auth-app composer install
 docker-compose exec auth-app cp .env.example .env
 docker-compose exec auth-app php artisan key:generate --ansi
 docker-compose exec auth-app bash -c "id -u > user_id.txt"
 docker-compose exec --user=root auth-app bash -c "read -r USER_ID<user_id.txt && chgrp -R ${USER_ID} storage bootstrap/cache"
-docker-compose exec --user=root auth-app bash chmod -R ug+rwx storage bootstrap/cache
+docker-compose exec --user=root auth-app chmod -R ug+rwx storage bootstrap/cache
 docker-compose exec auth-app php artisan passport:keys
 docker-compose exec auth-app php artisan migrate --seed
 docker-compose exec auth-app php artisan passport:client --password --no-interaction
+
+docker-compose exec films-app composer install
+docker-compose exec films-app cp .env.example .env
+docker-compose exec films-app php artisan key:generate --ansi
+docker-compose exec films-app bash -c "id -u > user_id.txt"
+docker-compose exec --user=root films-app bash -c "read -r USER_ID<user_id.txt && chgrp -R ${USER_ID} storage bootstrap/cache"
+docker-compose exec --user=root films-app chmod -R ug+rwx storage bootstrap/cache
+docker-compose exec films-app php artisan migrate --seed
 
 echo "Done"
--- films-app/config/app.php
+++ films-app/config/app.php
@@ -155,6 +155,7 @@ return [
         Illuminate\Queue\QueueServiceProvider::class,
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
+        Illuminate\Session\SessionServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
@@ -168,6 +169,8 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
+        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
