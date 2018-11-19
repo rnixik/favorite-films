8cd8a73b27311eb04b9b9f71ad9eea0c71a4c11b
Roman Nix <rnix@yandex.ru>
2018-11-19 09:36:46 +0700

    Fix order of operations in dev_deploy.sh

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -2,8 +2,8 @@
 
 set -e -o xtrace
 
-docker-compose exec auth-app composer install
 docker-compose exec auth-app cp .env.example .env
+docker-compose exec auth-app composer install
 docker-compose exec auth-app php artisan key:generate --ansi
 docker-compose exec auth-app bash -c "id -u > user_id.txt"
 docker-compose exec --user=root auth-app bash -c 'read -r USER_ID<user_id.txt && chgrp -R ${USER_ID} storage bootstrap/cache && echo ${USER_ID}'
@@ -12,8 +12,8 @@ docker-compose exec auth-app php artisan passport:keys
 docker-compose exec auth-app php artisan migrate --seed
 docker-compose exec auth-app php artisan passport:client --password --no-interaction
 
-docker-compose exec films-app composer install
 docker-compose exec films-app cp .env.example .env
+docker-compose exec films-app composer install
 docker-compose exec films-app php artisan key:generate --ansi
 docker-compose exec films-app bash -c "id -u > user_id.txt"
 docker-compose exec --user=root films-app bash -c 'read -r USER_ID<user_id.txt && chgrp -R ${USER_ID} storage bootstrap/cache && echo ${USER_ID}'
